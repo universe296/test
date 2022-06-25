fb4a5d02a2b24650167330f6ae8796719094f93e
longjie.chen <password@123>
2022-06-25 14:58:35 +0800

    1

---
+++ Euruka/src/main/java/com/test/euruka/EurukaApplication.java
@@ -0,0 +1,15 @@
+package com.test.euruka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurukaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurukaApplication.class, args);
+    }
+
+}
