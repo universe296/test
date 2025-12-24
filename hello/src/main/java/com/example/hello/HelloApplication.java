444d8178c194211ff2e061653707aaebe5a0921c
LongJieChen <LongJieChen@kaifa.cn>
2025-12-24 10:24:02 +0800

    1

--- hello/src/main/java/com/example/hello/HelloApplication.java
+++ hello/src/main/java/com/example/hello/HelloApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class HelloApplication {
-
+//
     public static void main(String[] args) {
         SpringApplication.run(HelloApplication.class, args);
     }
